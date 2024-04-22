a366865a8df8386fe709d518b80754a7cd0890bf
jyotianeja99 <jyotianeja99@gmail.com>
2024-04-22 13:43:57 +0530

    first commit

---
+++ src/numbers/PrintNum.java
@@ -0,0 +1,10 @@
+package numbers;
+
+public class PrintNum {
+    public static void main(String[] args) {
+        //Print nums
+        for(int i=0;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+}
